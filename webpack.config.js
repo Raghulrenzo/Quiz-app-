f9ac6cb57fc38f84466eb1efd5a7a26d22e4b111
raghul <raghulragu97@gmail.com>
2022-11-10 21:10:43 +0530

    plugin updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,42 @@
 const { dirname } = require('path');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "./dist"),
-        publicPath: './dist'
     },
     mode: 'development',
     module: {
         rules: [
           {
-            test: /\.s[ac]ss$/i,
+            test:/\.s[ac]ss$/i,
             use: [
+              MiniCssExtractPlugin.loader,
               "css-loader",
               "sass-loader",
             ],
           },
         ],
       },
-
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 9000,
+    },  
+   plugins: [
+    new MiniCssExtractPlugin({filename:"[name].css"}),
+    new HtmlWebpackPlugin(
+      {
+        title:'webpack',
+        template:'./src/index.html'
+      }
+    )
+    ]
+   
 }
